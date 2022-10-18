f62686f87e8e92c1249df4a8c76e8c0938158edf
rrayachhetri <rrayachhetri@gmail.com>
2022-10-18 10:53:09 -0700

    inquirer

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js')
 
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
-const pageHtml = generatePage(name, github);
-fs.writeFile('index.html', pageHtml, err => {
-    if (err) throw new Error(err);
-    console.log("Portfolio complete! Checkout index.html to see output");
-});
+import inquirer from 'inquirer';
+
+inquirer.prompt ([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+]).then(answers=> console.log(answers))
+
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js')
+// we are not using process.argv anymore because we installed inquirer  to do the same task
+
+// const pageHtml = generatePage(name, github);
+// fs.writeFile('index.html', pageHtml, err => {
+//     if (err) throw new Error(err);
+//     console.log("Portfolio complete! Checkout index.html to see output");
+// });
